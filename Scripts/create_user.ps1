ff612d0567c3f6a8d9ac8dbe9f3d4beed9569b0f
NilsEngholm <Nils.engholm@gmail.com>
2024-08-26 16:14:52 -0600

    added more param splatting, formatting, and domain detection

--- Scripts/create_user.ps1
+++ Scripts/create_user.ps1
@@ -1,5 +1,5 @@
 #import AD module
-$requiredModules = @('ActiveDirectory', 'powershell-yaml')
+$requiredModules = @('ActiveDirectory', 'powershell-yaml', 'get-culture')
 
 foreach ($module in $requiredModules) {
     if (Get-Module -ListAvailable -Name $requiredModules) {
@@ -10,6 +10,10 @@ foreach ($module in $requiredModules) {
         Install-Module -Name $module
     }
 }
+
+$textInfo = (get-culture).TextInfo
+$test = 'tEstInG'
+$textInfo.TotitleCase($test)
 
 #in this section we import user data | I've used a YAML file for this example, but will make a form later
 #this option allows the user to just type in the name of the file they want to use
@@ -22,6 +26,13 @@ Write-Host "Reading form file from $path..."
 $path = 'Scripts\john_doe.yaml'
 Write-Host "Reading form file from $path..."
 
+#verify domain info 
+$adDomain = Get-ADDomain
+$domainPrefix = $adDomain.DistinguishedName.Split(",")[0] -replace "DC=", ""
+$topLevelDomain = $adDomain.DistinguishedName.Split(",")[1] -replace "DC=", ""
+$domain = $domainPrefix + "." + $topLevelDomain
+Read-Host "Current AD domain: @$domain | Press enter to continue"
+
 try {
     $yaml = ConvertFrom-Yaml (Get-Content -Raw -Path $path)
 } catch {
@@ -29,26 +40,29 @@ try {
     return
 }
 
-#in this section we create the user values and splat them for entry to the new-aduser cmdlet
+#in this section we create the user values and splat them for entry to the new-aduser cmdlet | I've used the textinfo class to make the names title case but it's not necessary
+#this does not inculde the password, UPN, and group assignments | we will set that later
 $userParams = @{
-    Name = $yaml.GivenName + " " + $yaml.Surname
-    GivenName = $yaml.GivenName
-    Surname = $yaml.Surname
-    DisplayName = $yaml.givenName + " " + $yaml.Surname
-    SamAccountName = $yaml.givenName[0] + $yaml.Surname
-    UserPrincipalName = $yaml.UserPrincipalName
-    EmailAddress = $yaml.EmailAddress
-    Description = $yaml.jobTitle #in this example we'll display the job title of the user in their description field
-    Office = $yaml.city #in this example we'll display the location of the user in their office field
-    Department = $yaml.Department 
-    Title = $yaml.jobTitle #real job title section
-    Company = $yaml.company
-    Manager = $yaml.manager
-    #Path = $yaml.Path #OU path | leaving as default now but will add OU assignment later 
-    Enabled = $false
+    Name = $textinfo.ToTitleCase($yaml.GivenName + " " + $yaml.Surname)
+    GivenName = $textinfo.ToTitleCase($yaml.GivenName)
+    Surname = $textinfo.ToTitleCase($yaml.Surname)
+    SamAccountName = ($yaml.GivenName[0] + $yaml.Surname).ToLower()
+    DisplayName = $textInfo.ToTitleCase(($yaml.givenName + " " + $yaml.Surname))
+    EmailAddress = (($yaml.GivenName[0] + $yaml.Surname) + "@$domain").ToLower()
+    Description = $textinfo.ToTitleCase($yaml.jobTitle) #in this example we'll display the job title of the user in their description field
+    Office = $textinfo.ToTitleCase($yaml.city) #in this example we'll display the location of the user in their office field
+    Department = $textinfo.ToTitleCase($yaml.Department) 
+    Title = $textinfo.ToTitleCase($yaml.jobTitle) #real job title section
+    Company = $textinfo.ToTitleCase($yaml.company)
+    Manager = $yaml.manager #not title cased as this will use the samaccountname of the manager
+    Path = "OU=Test,OU=Test OU,DC=$domainPrefix,DC=$topLevelDomain"
+    Enabled = $false #this is set to false by default, the user will be enabled later
 }
 
-#perform error checking
+#add the UPN to the userParams | in this case we're using the email address as the UPN | this can be changed to a different format as needed
+$userParams.UserPrincipalName = $userParams.EmailAddress
+
+#perform duplicate check using samaccountname
 Write-Host "Checking if user $($userParams.SamAccountName) already exists..."
 try {
     Get-ADUser -Identity $userParams.SamAccountName
