d5fef14426253b5ab345450f78ef1b296dff5f43
NilsEngholm <Nils.engholm@gmail.com>
2024-08-26 18:27:45 -0600

    changed position of some blocks

--- Scripts/create_user.ps1
+++ Scripts/create_user.ps1
@@ -11,20 +11,8 @@ foreach ($module in $requiredModules) {
     }
 }
 
+#import culture info module for title casing
 $textInfo = (get-culture).TextInfo
-$test = 'tEstInG'
-$textInfo.TotitleCase($test)
-
-#in this section we import user data | I've used a YAML file for this example, but will make a form later
-#this option allows the user to just type in the name of the file they want to use
-'''
-$name = Read-Host "Enter the name of the file to use"
-$path = "C:\Users\Administrator\Documents\MACD YML\$name.yml"
-Write-Host "Reading form file from $path..."
-'''
-
-$path = 'Scripts\john_doe.yaml'
-Write-Host "Reading form file from $path..."
 
 #verify domain info 
 $adDomain = Get-ADDomain
@@ -33,12 +21,85 @@ $topLevelDomain = $adDomain.DistinguishedName.Split(",")[1] -replace "DC=", ""
 $domain = $domainPrefix + "." + $topLevelDomain
 Read-Host "Current AD domain: @$domain | Press enter to continue"
 
+#in this section we import user data | I've used a YAML file for this example, but will make a form later
+#this option allows the user to just type in the name of the file they want to use
+
+$name = Read-Host "Enter the name of the file to use"
+$path = "C:\Users\Administrator\Documents\MACD YML\$name.yml"
+Write-Host "Reading form file from $path..."
+
+
+#$path = 'Scripts\john_doe.yaml'
+Write-Host "Reading form file from $path..."
+
 try {
     $yaml = ConvertFrom-Yaml (Get-Content -Raw -Path $path)
 } catch {
     Write-Error "Failed to read the YAML file. Please check the file path and format."
     return
 }
+
+#here we create the form to input user data
+function Get-ADUserForm {
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    # The object
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Active Directory User Information"
+    $form.Size = New-Object System.Drawing.Size(400, 400)
+
+    # Submit button
+    $submitButton = New-Object System.Windows.Forms.Button
+    $submitButton.Text = "Submit"
+    $submitButton.Location = New-Object System.Drawing.Point(260, 310)
+    $submitButton.Size = New-Object System.Drawing.Size(100, 30)
+    $form.Controls.Add($submitButton)
+
+    # Form labels and text boxes
+    $labels = @("First Name:", "Last Name:", "Phone:", "Location:", "Job Title:", "Manager:", "Division:")
+    $textBoxes = @()
+    $yPos = 45
+
+    foreach ($labelText in $labels) {
+        $label = New-Object System.Windows.Forms.Label
+        $label.Text = $labelText
+        $label.Location = New-Object System.Drawing.Point(10, $yPos)
+        $form.Controls.Add($label)
+
+        $textBox = New-Object System.Windows.Forms.TextBox
+        $textBox.Location = New-Object System.Drawing.Point(120, $yPos)
+        $textBox.Size = New-Object System.Drawing.Size(225, 20)
+        $form.Controls.Add($textBox)
+        $textBoxes += $textBox
+
+        $yPos += 40
+    }
+
+        # Add functionality to the submit button
+        $submitButton.Add_Click({
+            # Create a hash table using the form data
+            $values = @(
+                $textBoxes[0].Text,
+                $textBoxes[1].Text,
+                $textBoxes[2].Text,
+                $textBoxes[3].Text,
+                $textBoxes[4].Text,
+                $textBoxes[5].Text,
+                $textBoxes[6].Text
+            )
+            $form.Tag = $values
+            $form.Close()
+        })
+
+        # Initiate the form
+        $form.ShowDialog()
+        
+        # Return form data as a list
+        return $form.Tag
+}
+
+$yaml = Get-ADUserForm
 
 #in this section we create the user values and splat them for entry to the new-aduser cmdlet | I've used the textinfo class to make the names title case but it's not necessary
 #this does not inculde the password, UPN, and group assignments | we will set that later
